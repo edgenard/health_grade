92e25f114a90ae7db0045a49fbec4bd89214dae2
Emmanuel Genard <edgenard@gmail.com>
2019-07-28 19:03:41 -0400

    make primary key on restaurants table, camis, bigint

---
+++ db/migrate/20190728225908_make_camis_big_int.rb
@@ -0,0 +1,5 @@
+class MakeCamisBigInt < ActiveRecord::Migration[5.2]
+  def change
+    change_column :restaurants, :camis, :bigint
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_20_202515) do
+ActiveRecord::Schema.define(version: 2019_07_28_225908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2019_07_20_202515) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "restaurants", primary_key: "camis", id: :serial, force: :cascade do |t|
+  create_table "restaurants", primary_key: "camis", force: :cascade do |t|
     t.string "name"
     t.string "boro"
     t.integer "building"
