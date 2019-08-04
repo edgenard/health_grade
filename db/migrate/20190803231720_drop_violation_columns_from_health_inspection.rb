13bc694636e5816575ff00e0de425e8416188210
Emmanuel Genard <edgenard@gmail.com>
2019-08-03 20:51:46 -0400

    fix errors in drop column migration

--- db/migrate/20190803231720_drop_violation_columns_from_health_inspection.rb
+++ db/migrate/20190803231720_drop_violation_columns_from_health_inspection.rb
@@ -4,9 +4,9 @@ class DropViolationColumnsFromHealthInspection < ActiveRecord::Migration[5.2]
   # rubocop:disable Rails/ReversibleMigration
   def change
     change_table :health_inspections, bulk: true do |t|
-      t.remove :violation_code, :string
-      t.remove :violation_description, :text
-      t.remove :critical_flat, :string
+      t.remove :violation_code
+      t.remove :violation_description
+      t.remove :critical_flag
     end
   end
   # rubocop:enable Rails/ReversibleMigration
